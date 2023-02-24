c1afd5e578af780f013abbb8103b2c9edf9feea2
jufrin01 <125728570+jufrin01@users.noreply.github.com>
2023-02-24 08:35:18 +0700

    "jufrin"

---
+++ Pengulangan0.java
@@ -0,0 +1,10 @@
+public class Pengulangan0{
+    public void main(String[] args) {
+        int a , b, c ;
+
+        for (a=0; a<=7; a++){
+
+            System.out.println(a+"angka1");
+        }
+    }
+}
---
+++ Perulangan0.java
@@ -0,0 +1,14 @@
+public class Perulangan0 {
+    public static void main(String[] args) {
+        int a ,b ;
+        for (a = 10; a >=0 ; a--)
+        {
+            System.out.println( );
+
+            for (b =20; b > a ; b--){
+                System.out.print(b+" ");
+
+            }
+        }
+    }
+}
